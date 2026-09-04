d9e614f59e81ece35e04c9220609c8fe305e9fd9
neiladeiro-analista <neiladeiro@gmail.com>
2026-09-04 19:46:20 -0300

    feat: tela de login

---
+++ app.js
@@ -0,0 +1,20 @@
+function calcularTotal(itens) {
+
+   let total = 0
+
+   for (let i = 0; i < itens.length; i++) {
+
+       total += itens[i].preco 
+
+   }
+
+   // aplica desconto de fidelidade
+  // antes de retornar o valor final
+
+   return total
+
+}
+
+
+
+ 
